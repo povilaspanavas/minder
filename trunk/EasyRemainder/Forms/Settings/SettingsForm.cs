26916167ba0dfb04f3d6fe9766ace31230b16d7f
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2012-06-13 20:41:08 +0000

    Pridėjau settings formą ir preparerį

---
+++ trunk/EasyRemainder/Forms/Settings/SettingsForm.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: IGNAS
+ * Date: 2012.06.13
+ * Time: 23:36
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EasyRemainder.Forms.Settings
+{
+	/// <summary>
+	/// Description of SettingsForm.
+	/// </summary>
+	public partial class SettingsForm : Form
+	{
+		public SettingsForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
