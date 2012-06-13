5dcf0e8b8c690ec9c3dfd0cb1dd402a96df392c4
povilas.panavas <povilas.panavas@gmail.com>
2012-06-13 19:46:32 +0000

    Pirmoji versija code.google.com svetainėj

---
+++ EasyRemainder/BootStrap.cs
@@ -0,0 +1,104 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Autogidas_checker.Forms.Main;
+using Core;
+using Core.Forms;
+using Core.Forms.LoginForm;
+using Core.Tools.Log;
+using Core.Web;
+using EasyRemainder.Forms.Main;
+using EasyRemainder.Objects;
+using Minder.Sql;
+
+namespace EasyRemainder
+{
+	public class MainController
+	{
+		
+	}
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class BootStrap
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Starter();
+//			StartEngine();
+			Application.Run();
+		}
+		
+		private static void StartEngine()
+		{
+			Timer timer = new Timer();
+			timer.Interval = 7000;
+			timer.Tick += delegate {
+				using (DBConnection connection = new DBConnection())
+				{
+					List<Task> tasksToShow = connection.LoadData();
+					foreach (Task task in tasksToShow) {
+						System.Windows.Forms.MessageBox.Show(
+							string.Format("Pavadinimas: {0}, Laikas:{1}", 
+							              task.Name, DBTypesConverter.ToFullDateString(task.DateRemainder)));
+					}
+					foreach (Task task in tasksToShow) {
+						connection.Delete(task.Id);
+					}
+					connection.SelectFirstTask();
+				}
+			};
+			timer.Start();
+			
+			Main_From_Preparer preparer = new Main_From_Preparer();
+			preparer.PrepareForm();
+			preparer.DataEntered += delegate(string dataEntered) { 
+				timer.Stop();
+//				timer.Dispose();
+				string name = dataEntered.Substring(0, dataEntered.IndexOf("|"));
+				string time = dataEntered.Substring(dataEntered.IndexOf("|") + 1);
+				int minutes = int.Parse(time);
+				DateTime showTime = DateTime.Now.AddMinutes(minutes);
+				using (DBConnection connection = new DBConnection())
+				{
+					connection.ExecuteNonQuery("INSERT INTO TASK (NAME, EXECUTETIME) VALUES('" +
+					                           name + "', '" + string.Format("{0:yyyy.MM.dd HH:mm:ss}", showTime) + "')");
+					timer.Interval = connection.SelectNextInterval();
+					timer.Tick += delegate {
+						using (DBConnection connection2 = new DBConnection())
+						{
+							List<Task> tasksToShow = connection2.LoadData();
+							foreach (Task task in tasksToShow) {
+								System.Windows.Forms.MessageBox.Show(task.Name);
+							}
+							foreach (Task task in tasksToShow) {
+								connection2.Delete(task.Id);
+							}
+						}
+					};
+					timer.Start();
+				}
+			};
+		}
+		
+		private static void Starter()
+		{
+			ConfigLoader.LoadConfiguration(false);
+			new UpdateVersion();
+			Loger loger = new Loger();
+			loger.LogWrite(string.Format("Program started \n Program version: {0}",
+			                             StaticData.VersionCache.Version), LogerType.Info);
+			
+			Main_From_Preparer preparer = new Main_From_Preparer();
+			preparer.PrepareForm();
+		}
+		
+	}
+}
---
+++ EasyRemainder/Forms/Main/Main_From_Preparer.cs
@@ -0,0 +1,78 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+using Autogidas_checker.Forms.Main;
+using Core;
+using Core.Forms;
+using Core.Tools.GlobalHotKeys;
+using Core.Tools.ImportExport;
+using Core.Tools.Log;
+
+namespace EasyRemainder.Forms.Main
+{
+	/// <summary>
+	/// Description of Main_From_Preparer.
+	/// </summary>
+	public class Main_From_Preparer : IFormPreparer
+	{
+		private Main_Form m_form = null;
+		public event TaskData DataEntered;
+		public delegate void TaskData(string dataEntered);
+		
+		public Main_From_Preparer()
+		{
+			m_form = new Main_Form();
+		}
+
+		public void PrepareForm()
+		{
+			BackroundWorks();
+			SetEvents();
+		}
+
+		public void SetEvents()
+		{
+			
+		}
+		
+		private void BackroundWorks()
+		{
+			RegisterHotKeys();
+		}
+		
+		private void RegisterHotKeys()
+		{
+			HotKeys hotKeys = new HotKeys();
+			hotKeys.KeyPressed += new EventHandler<KeyPressedEventArgs>(KeyPressed);
+			hotKeys.RegisterHotKey(ModifierKeys.Alt, Keys.N);
+			m_form.m_textBox.KeyDown += KeyPressedEnter;
+		}
+		
+		private void KeyPressed(object sender, KeyPressedEventArgs e)
+		{
+			if(m_form.Visible == true)
+				m_form.Visible = false;
+			else
+				m_form.Visible = true;
+		}
+		
+		private void KeyPressedEnter(object sender, KeyEventArgs e)
+		{
+			if(e.Control == false || e.KeyCode != Keys.Enter)
+				return;
+			if (DataEntered != null)
+				DataEntered(this.m_form.TextBox.Text);
+			m_form.Visible = false;
+		}
+		
+		public Form Form {
+			get { return m_form;}
+		}
+	}
+}
