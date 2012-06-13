f46361ce1351aa5ad381ca67e753d28bb976b053
ign.bgd <ign.bgd@gmail.com>
2012-06-13 20:41:08 +0000

    Pridėjau settings formą ir preparerį

---
+++ EasyRemainder/Forms/Settings/SettingsFromPreparer.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: IGNAS
+ * Date: 2012.06.13
+ * Time: 23:38
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Core.Forms;
+
+namespace EasyRemainder.Forms.Settings
+{
+	/// <summary>
+	/// Description of SettingsFromPreparer.
+	/// </summary>
+	public class SettingsFromPreparer : IFormPreparer
+	{
+		SettingsForm m_form = null;
+		
+		public SettingsFromPreparer()
+		{
+			m_form = new SettingsForm();
+			m_form.Text = "Settings";
+		}
+		
+		public System.Windows.Forms.Form Form {
+			get {
+				return m_form;
+			}
+		}
+		
+		public void PrepareForm()
+		{
+			
+		}
+		
+		public void SetEvents()
+		{
+			
+		}
+	}
+}
