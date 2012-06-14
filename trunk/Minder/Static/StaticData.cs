d7d8f55e3287e4efa93c862239799baf714a8145
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2012-06-14 21:33:15 +0000

    Pridėjau Dictionary kuriame saugomi key'ai.

--- trunk/Minder/Static/StaticData.cs
+++ trunk/Minder/Static/StaticData.cs
@@ -1,5 +1,7 @@
 ﻿
 using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace EasyRemainder.Static
 {
@@ -8,6 +10,72 @@ namespace EasyRemainder.Static
 	/// </summary>
 	public static class StaticData
 	{
+		private static Dictionary<string, Keys> m_keysDic = new Dictionary<string, Keys>();
 		
+		public static Dictionary<string, Keys> KeysDic 
+		{
+			get 
+			{
+				AddKeysToDic();
+				return m_keysDic; 
+			}
+		}
+		
+		private static void AddKeysToDic()
+		{
+			if(m_keysDic.Count != 0)
+				return;
+			
+			m_keysDic.Add("F1", Keys.F1);
+			m_keysDic.Add("F2", Keys.F2);
+			m_keysDic.Add("F3", Keys.F3);
+			m_keysDic.Add("F4", Keys.F4);
+			m_keysDic.Add("F5", Keys.F5);
+			m_keysDic.Add("F6", Keys.F6);
+			m_keysDic.Add("F7", Keys.F7);
+			m_keysDic.Add("F8", Keys.F8);
+			m_keysDic.Add("F9", Keys.F9);
+			m_keysDic.Add("F10", Keys.F10);
+			m_keysDic.Add("F11", Keys.F11);
+			m_keysDic.Add("F12", Keys.F12);
+			
+			m_keysDic.Add("A", Keys.A);
+			m_keysDic.Add("B", Keys.B);
+			m_keysDic.Add("C", Keys.C);
+			m_keysDic.Add("D", Keys.D);
+			m_keysDic.Add("E", Keys.E);
+			m_keysDic.Add("F", Keys.F);
+			m_keysDic.Add("G", Keys.G);
+			m_keysDic.Add("H", Keys.H);
+			m_keysDic.Add("I", Keys.I);
+			m_keysDic.Add("Y", Keys.Y);
+			m_keysDic.Add("J", Keys.J);
+			m_keysDic.Add("K", Keys.K);
+			m_keysDic.Add("L", Keys.L);
+			m_keysDic.Add("M", Keys.M);
+			m_keysDic.Add("N", Keys.N);
+			m_keysDic.Add("P", Keys.P);
+			m_keysDic.Add("R", Keys.R);
+			m_keysDic.Add("S", Keys.S);
+			m_keysDic.Add("Q", Keys.Q);
+			m_keysDic.Add("T", Keys.T);
+			m_keysDic.Add("U", Keys.U);
+			m_keysDic.Add("V", Keys.V);
+			m_keysDic.Add("W", Keys.W);
+			m_keysDic.Add("X", Keys.X);
+			m_keysDic.Add("Z", Keys.Z);
+			
+			m_keysDic.Add("0", Keys.NumPad0);
+			m_keysDic.Add("1", Keys.NumPad1);
+			m_keysDic.Add("2", Keys.NumPad2);
+			m_keysDic.Add("3", Keys.NumPad3);
+			m_keysDic.Add("4", Keys.NumPad4);
+			m_keysDic.Add("5", Keys.NumPad5);
+			m_keysDic.Add("6", Keys.NumPad6);
+			m_keysDic.Add("7", Keys.NumPad7);
+			m_keysDic.Add("8", Keys.NumPad8);
+			m_keysDic.Add("9", Keys.NumPad9);
+			
+		}
 	}
 }
