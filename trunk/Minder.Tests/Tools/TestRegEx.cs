6f8f3fd9071ada229d5ed622df7f86a29820c215
povilas.panavas@gmail.com <povilas.panavas@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2012-06-16 10:06:00 +0000

    Išsitestavau Regex klasę :)

---
+++ trunk/Minder.Tests/Tools/TestRegEx.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Povilas
+ * Date: 2012.06.16
+ * Time: 12:40
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace Minder.Tests.Tools
+{
+	[TestFixture]
+	public class TestRegEx
+	{
+		[Test]
+		public void TestMethod()
+		{
+			Assert.AreEqual("15m", Regex.Match("Nupirkti pieno 15m", @"\d*m$").Value);
+			Assert.AreEqual("5m", Regex.Match("Nupirkti pieno 5m", @"\d*m$").Value);
+			Assert.AreEqual("1234min", Regex.Match("Nupirkti pieno 1234min", @"\d*min$").Value);
+			Assert.AreEqual("2m", Regex.Match("Nupirkti pieno 2m", @"\d*m$").Value);
+			
+			// Turi būti gale
+			Assert.IsFalse(Regex.IsMatch("Nupirkti pieno 15m blabla", @"\d*m$"));
+			Assert.IsFalse(Regex.IsMatch("Nupirkti pieno 15m blabla", @"\d*m$"));
+			
+			// Parenkamas iš galo jei yra du
+			Assert.AreEqual("21m", Regex.Match("Nupirkti pieno 15m blabla 21m", @"\d*m$").Value);
+			
+		}
+	}
+}
