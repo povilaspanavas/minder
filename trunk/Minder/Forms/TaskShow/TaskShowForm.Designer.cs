9c052ed153c6209838f7b471fb7cdfcea8d9e460
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2012-06-17 15:50:54 +0000

    Padariau formą taskų rodymui, bet kažkas negerai su update'u.

---
+++ trunk/Minder/Forms/TaskShow/TaskShowForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: IGNAS
+ * Date: 2012.06.17
+ * Time: 18:00
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Minder.Forms.TaskShow
+{
+	partial class TaskShowForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TaskShowForm));
+			this.m_remainderMeLaterButton = new System.Windows.Forms.Button();
+			this.m_okButton = new System.Windows.Forms.Button();
+			this.m_textBox = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// m_remainderMeLaterButton
+			// 
+			this.m_remainderMeLaterButton.Location = new System.Drawing.Point(178, 89);
+			this.m_remainderMeLaterButton.Name = "m_remainderMeLaterButton";
+			this.m_remainderMeLaterButton.Size = new System.Drawing.Size(107, 30);
+			this.m_remainderMeLaterButton.TabIndex = 0;
+			this.m_remainderMeLaterButton.Text = "Remind me later";
+			this.m_remainderMeLaterButton.UseVisualStyleBackColor = true;
+			// 
+			// m_okButton
+			// 
+			this.m_okButton.Location = new System.Drawing.Point(52, 89);
+			this.m_okButton.Name = "m_okButton";
+			this.m_okButton.Size = new System.Drawing.Size(107, 30);
+			this.m_okButton.TabIndex = 2;
+			this.m_okButton.Text = "Ok";
+			this.m_okButton.UseVisualStyleBackColor = true;
+			// 
+			// m_textBox
+			// 
+			this.m_textBox.Location = new System.Drawing.Point(12, 20);
+			this.m_textBox.Name = "m_textBox";
+			this.m_textBox.Size = new System.Drawing.Size(315, 66);
+			this.m_textBox.TabIndex = 3;
+			this.m_textBox.Text = "Task: task description\r\n\r\nTime: task time";
+			// 
+			// TaskShowForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(339, 140);
+			this.Controls.Add(this.m_textBox);
+			this.Controls.Add(this.m_okButton);
+			this.Controls.Add(this.m_remainderMeLaterButton);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "TaskShowForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "TaskShowForm";
+			this.TopMost = true;
+			this.ResumeLayout(false);
+		}
+		public System.Windows.Forms.Button m_okButton;
+		public System.Windows.Forms.Label m_textBox;
+		public System.Windows.Forms.Button m_remainderMeLaterButton;
+	}
+}
---
+++ trunk/Minder/Forms/TaskShow/TaskShowFormPreparer.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: IGNAS
+ * Date: 2012.06.17
+ * Time: 18:01
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Core.Forms;
+using Minder.Objects;
+using Minder.Sql;
+
+namespace Minder.Forms.TaskShow
+{
+	/// <summary>
+	/// Description of TaskShowFormPreparer.
+	/// </summary>
+	public class TaskShowFormPreparer : IFormPreparer
+	{
+		TaskShowForm m_form = null;
+		
+		Task m_task;
+		
+		public Task Task {
+			get { return m_task; }
+			set { m_task = value; }
+		}
+		
+		public TaskShowFormPreparer(Task task)
+		{
+			m_form = new TaskShowForm();
+			m_form.Text = "Minder Task";
+			m_task = task;
+		}
+		
+		public System.Windows.Forms.Form Form 
+		{
+			get {
+				return m_form;
+			}
+		}
+		
+		public void PrepareForm()
+		{
+			SetEvents();
+			SetTaskText();
+			m_form.ShowDialog();
+		}
+		
+		public void SetEvents()
+		{
+			m_form.m_okButton.Click += delegate 
+			{ 
+				m_task.Showed = true;
+				m_task.Update();
+				m_form.Close();
+			};
+			
+			m_form.m_remainderMeLaterButton.Click += delegate 
+			{
+//				m_task.Showed = false;
+				m_task.DateRemainder.AddMinutes(5); //Temp
+				m_task.Update();
+				m_form.Close();
+			};
+		}
+		
+		private void SetTaskText()
+		{
+			if(m_task == null)
+				return;
+			m_form.m_textBox.Text = 
+				string.Format("Task: {0}{1}{2}Time: {3}", m_task.Text, Environment.NewLine, Environment.NewLine,  
+				              DBTypesConverter.ToFullDateString(m_task.DateRemainder));
+		}
+	}
+}
