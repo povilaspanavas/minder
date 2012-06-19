2a16cbec7bb0ea0ee3bd8e7d28e28b8f20a6f404
ign.bgd <ign.bgd@gmail.com>
2012-06-19 20:39:11 +0000

    Patvarkytas installo gaminimas.

--- Install/BuildAndCopyInstallFiles.bat
+++ Install/BuildAndCopyInstallFiles.bat
@@ -3,7 +3,18 @@ path = "..\Minder\Minder.csproj"
 
 "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild" %path% /t:Rebuild >> Build.log && type Build.log
 
-if exist "..\Minder\bin\debug\log" RD "..\Minder\bin\debug\log" /S /Q
-DEL "Builded installs\*.*" /Q 
+rem if exist "..\Minder\bin\debug\log" RD "..\Minder\bin\debug\log" /S /Q
+rem DEL "Builded installs\*.*" /Q 
+rd "Install Files" /s/q
+md "Install Files"
+md "Install Files/bin/files"
+md "Install Files/DB"
 
+copy "..\Minder\bin\debug\*.*" "Install Files\bin\files"
+copy "..\Minder\DB\*.*" "Install Files\DB"
+if exist "Install Files\bin\log" RD "..\Minder\bin\debug\log" /S /Q
+del "Install Files\DB\minderdb.db" /Q
+del "Install Files\bin\files\*.pdb" /Q
+del "Install Files\bin\files\*.ini" /Q
+copy "Install Files\DB\emptyminderdb.db" "Install Files\DB\MinderDb.db"
 pause
