f8fb7fa4f8394c6cd92c8efbb26e9277cdbd775f
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2012-09-08 13:05:17 +0000

    Konfiguruojami web servisai

--- tags/Minder_0.0.1.275/Minder.WebServices/Soap.cs
+++ tags/Minder_0.0.1.275/Minder.WebServices/Soap.cs
@@ -11,41 +11,21 @@ using System.Data;
 using System.Web;
 using System.Web.Services;
 using System.Web.Services.Protocols;
+using System.Web.Script.Services;
 
 namespace Minder.WebServices
 {
-	[WebService]
+	[WebService(Namespace="Minder")]
+	[ScriptService]
 	public class Soap : System.Web.Services.WebService
 	{
-		/// <summary>
-		/// Logs into the web service
-		/// </summary>
-		/// <param name="userName">The User Name to login in as</param>
-		/// <param name="password">User's password</param>
-		/// <returns>True on successful login.</returns>
+		
 		[WebMethod(EnableSession=true)]
+		[ScriptMethod(ResponseFormat = ResponseFormat.Json)]
 		public bool Login(string userName, string password)
 		{
-			//NOTE: There are better ways of doing authentication. This is just illustrates Session usage.
-			UserName = userName;
-			return true;
+			return false;
 		}
 		
-		/// <summary>
-		/// Logs out of the Session.
-		/// </summary>
-		[WebMethod(EnableSession=true)]
-		public void Logout()
-		{    
-			Context.Session.Abandon();
-		}
-		
-		/// <summary>
-		/// UserName of the logged in user.
-		/// </summary>
-		private string UserName {
-			get {return (string)Context.Session["User"];}
-			set {Context.Session["User"] = value;}
-		}
 	}
 }
