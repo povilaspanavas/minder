cadc66630c59ac1eeac91142945542c94efddafe
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2012-09-15 19:06:54 +0000

    Pataisytas testas su versijomis

--- tags/Minder_0.0.1.275/Minder.Tests/DB/TestDBVersionSystem.cs
+++ tags/Minder_0.0.1.275/Minder.Tests/DB/TestDBVersionSystem.cs
@@ -21,13 +21,9 @@ namespace Minder.Tests.DB
 	[DBHistory]
 	public class TestDBVersionsFile
 	{
-		[DBVersion(1, "Adds DBVersion's table", "2012.09.15 00:16:00")]
+		[DBVersion(1, "Adds DBVersion table", "2012.09.15 00:16:00")]
 		public void Version()
 		{
-			try {
-				GenericDbHelper.DropAllTables();
-			} catch (Exception) {
-			}
 			GenericDbHelper.RunDirectSql("CREATE TABLE TROL_TEST (ID INTEGER PRIMARY KEY, VERSION_NR INTEGER NOT NULL, " +
 			                             "COMMENT VARCHAR(255))");
 			GenericDbHelper.RunDirectSql("INSERT INTO TROL_TEST (VERSION_NR, COMMENT) VALUES (5, \"Komentaras\")");
@@ -42,13 +38,12 @@ namespace Minder.Tests.DB
 		{
 			ConfigLoader.Load(@"CoreConfig.xml");
 //			DbHelper.TestMode = true;
-//			try
-//			{
-//				using (IConnection con = new ConnectionCollector().GetConnection())
-//				{
-//					new DbHelper().CreateTable();
-//				}
-//			} catch (Exception ex) { ex.ToString(); }
+			try 
+			{
+				GenericDbHelper.DropAllTables();
+				GenericDbHelper.CreateTable(typeof(DBVersion));
+			} 
+			catch{}
 		}
 		
 		[Test]
@@ -66,7 +61,7 @@ namespace Minder.Tests.DB
 			DBVersionRepository repo = new DBVersionRepository()
 				.AddVersions(typeof(TestDBVersionSystem).Assembly)
 				;
-			
+//			GenericDbHelper.CreateTable(typeof(DBVersion));
 			new DBVersionSystemRunner().ExecuteUpdateToDB(repo);
 			using (IConnection con = new ConnectionCollector().GetConnection())
 			{
