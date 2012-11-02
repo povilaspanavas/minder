d4d0cb1fd8ad535c42f9056d5d5c14da74dc15d3
povilas.panavas <povilas.panavas@gmail.com>
2012-11-02 11:51:33 +0000

    Jau juoda skinas ir baltas tekstas

--- Minder/UI/SkinController/MainForms/BlackSkin/BlackSkin.xaml.cs
+++ Minder/UI/SkinController/MainForms/BlackSkin/BlackSkin.xaml.cs
@@ -1,15 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Minder.UI.SkinController.MainForms
 {
@@ -19,13 +9,13 @@ namespace Minder.UI.SkinController.MainForms
     public partial class BlackSkin : AbstractMainForm
     {
     	public const string SKIN_UNIQUE_CODE = "BLACK_SKIN";
-    	
-        public BlackSkin()
+        
+		public BlackSkin()
         {
             InitializeComponent();
         }
-        
-         public override TextBox MTextBox {
+		
+		public override TextBox MTextBox {
 			get {
 				return m_textBox;
 			}
