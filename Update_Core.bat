69f3c03ae2f9325a935efe1842b4bc238b89924e
ign.bgd <ign.bgd@gmail.com>
2013-02-10 16:36:56 +0000

    Atnaujinau core

--- Update_Core.bat
+++ Update_Core.bat
@@ -5,11 +5,11 @@ echo Started Core update
 rem update core
 
 rem build core before copying
-call d:\Projektai\Core_tags\Core_2.0\Build.bat
+call d:\Projektai\Core_tags\Core_2.2\Build.bat
 
 rem copying
 cd "../../../"
-cd "d:\Projektai\Core_tags\Core_2.0\Core\bin\Debug\"
+cd "d:\Projektai\Core_tags\Core_2.2\Core\bin\Debug\"
 copy *.* "d:\Projektai\Minder_trunk\libs\"
 
 
