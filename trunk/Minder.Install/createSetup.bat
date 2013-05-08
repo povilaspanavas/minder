9e3730fc1e6f8dd1959ff53527f9a485eb0b1a11
povilas.panavas@gmail.com <povilas.panavas@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2013-05-08 21:42:30 +0000

    Jau pasigamina installas - liko jį ištestuoti ir sutvarkyti

--- trunk/Minder.Install/createSetup.bat
+++ trunk/Minder.Install/createSetup.bat
@@ -1,3 +1,7 @@
-%windir%\microsoft.net\framework\v4.0.30319\msbuild /p:Configuration=Debug /p:SharpDevelopBinPath="C:\Program Files (x86)\SharpDevelop\4.3\bin" ..\Minder.sln
+mkdir bin\
+del bin\*.* /Q
 
+%windir%\microsoft.net\framework\v4.0.30319\msbuild /p:Configuration=Release /p:SharpDevelopBinPath="C:\Program Files (x86)\SharpDevelop\4.3\bin" ..\Minder.sln
+
+"NSIS\makensis" "Scripts\minder.template"
 pause
