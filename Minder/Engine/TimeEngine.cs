5e176c2a1d0dc76bee7e5f877b7fd3119a72d96b
povilas.panavas <povilas.panavas@gmail.com>
2013-05-08 20:49:03 +0000

    Pridėta loginimo visokio, kad padėtų atsekti bugus

--- Minder/Engine/TimeEngine.cs
+++ Minder/Engine/TimeEngine.cs
@@ -13,11 +13,14 @@ namespace Minder.Engine
 {
 	public class TimeEngine
 	{
+		private static log4net.ILog m_log = log4net.LogManager.GetLogger(typeof(TimeEngine));
+
 		public static event TaskChanged TaskChangedEvent;
 		public delegate void TaskChanged(Task task);
 		
 		public static void FireTaskChangedEvent(Task task)
 		{
+			m_log.Debug("FireTaskChangedEvent was called");
 			if (TaskChangedEvent != null)
 				TaskChangedEvent(task);
 		}
