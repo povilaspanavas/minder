8ddc2633dccacc740308fcb4885c08b9a7e77f18
povilas.panavas@gmail.com <povilas.panavas@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2013-05-09 17:55:14 +0000

    Jau isntallo failo pavaidnime būna versijos

--- trunk/Minder.Install/createSetup.bat
+++ trunk/Minder.Install/createSetup.bat
@@ -3,5 +3,5 @@ del bin\*.* /Q
 
 %windir%\microsoft.net\framework\v4.0.30319\msbuild /p:Configuration=Release /p:SharpDevelopBinPath="C:\Program Files (x86)\SharpDevelop\4.3\bin" ..\Minder.sln
 
-"NSIS\makensis" "Scripts\minder.template"
+"NSIS\makensis" "Scripts\minder.nsi"
 pause
