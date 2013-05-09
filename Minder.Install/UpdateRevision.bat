975976fa949d06ea4717a7c185b6974772d24f42
povilas.panavas <povilas.panavas@gmail.com>
2013-05-09 19:24:07 +0000

    version.crv failas automatiškai atnaujinamas gaminant installą. Regis dabar jau viskas automatizuota :} Beliko automatizuoti naujo installo talpinimą į ftp ir į webą

--- Minder.Install/UpdateRevision.bat
+++ Minder.Install/UpdateRevision.bat
@@ -1,5 +1,7 @@
 RevisionUpdater\RevisionUpdater.exe --TagFile VersionFiles\REVISION --ReplaceRevision Scripts\minder.template --InsertRevision-- Scripts\minder.version --enc Default
 
+RevisionUpdater\RevisionUpdater.exe --TagFile VersionFiles\REVISION --ReplaceRevision ..\Minder\version.template --InsertRevision-- ..\Minder\version.version --enc Default
+
 REM RevisionUpdater\RevisionUpdater.exe --TagFile VersionFiles\REVISION --ReplaceRevision ..\PUV\RevisionClass.template --InsertRevision-- ..\PUV\RevisionClass.cs --enc Default
 
 rem %windir%\microsoft.net\framework\v3.5\msbuild /p:Configuration=Debug /p:SharpDevelopBinPath="C:\Program Files\SharpDevelop\3.0\bin" ..\PUV.sln
