ffb216dcc92c1bff5cb57e9e21f36504e2db3b08
povilas.panavas <povilas.panavas@gmail.com>
2013-05-09 18:08:00 +0000

    Komitins automatišai pasikeitusį revizijos numerį

--- Minder.Install/createSetup.bat
+++ Minder.Install/createSetup.bat
@@ -7,3 +7,7 @@ For /F "Tokens=*" %%I in ('svnversion') Do Set version=%%I
 echo %version% > REVISION
 popd
 call createSetupSimple.bat
+
+pushd ..\
+svn commit -m "#000 - atnaujinti versiju failai (paruostas atnaujinimas)."
+popd
