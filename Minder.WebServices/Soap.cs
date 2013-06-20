58602060d1a65763ab18b430fadb262baad03f62
ign.bgd <ign.bgd@gmail.com>
2013-06-20 15:21:57 +0000

    Pridetas trynimo metodas serveryje

--- Minder.WebServices/Soap.cs
+++ Minder.WebServices/Soap.cs
@@ -14,9 +14,10 @@ using System.Web;
 using System.Web.Script.Services;
 using System.Web.Services;
 using System.Web.Services.Protocols;
-
 using Core;
+using Core.DB;
 using Core.Tools.Log;
+using Minder.WebServices.Constants;
 using Minder.WebServices.Objects;
 using Minder.WebServices.Services;
 using PUV.WebServices.Helpers;
@@ -74,6 +75,45 @@ namespace Minder.WebServices
 				TimeSpan span = DateTime.Now - startTime;
 				log.InfoFormat("Successfully synced for user: {0} {1} seconds", userId, span.TotalSeconds);
 				return jsonResult;
+			}
+			catch (Exception e)
+			{
+				log.Error(e);
+				return e.ToString();
+			}
+		}
+		
+		[WebMethod(EnableSession=true)]
+		[ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+		public string DeleteUserTasks(string json)
+		{
+			log4net.ILog log = log4net.LogManager.GetLogger(typeof(Soap));
+			if(Minder.WebServices.Helpers.StaticData.ConfigLoaded == false)
+			{
+				ConfigLoader.Load(@"d:\Projektai\Minder_trunk\Minder.WebServices\bin\CoreConfig.xml");
+				FileInfo config = new FileInfo(@"d:\Projektai\Minder_trunk\Minder.WebServices\bin\MinderWebServices.log4net.xml");
+				log4net.Config.XmlConfigurator.Configure(config);
+				Minder.WebServices.Helpers.StaticData.ConfigLoaded = true;
+				log.Info("Service started...");
+			}
+
+			try
+			{
+				if(string.IsNullOrEmpty(json))
+					return string.Empty;
+	
+				DeleteObject deleteObject = JsonHelper.OnlyJsonToObject<DeleteObject>(json);
+				if(deleteObject == null)
+					return string.Empty;
+				
+				string userId = deleteObject.UserId;
+				string password = deleteObject.Password;
+				if(ServicesConstants.DELETE_PASSWORD.Equals(password) == false || 
+				   string.IsNullOrEmpty(userId))
+					return string.Empty;
+				
+				GenericDbHelper.RunDirectSql(string.Format("DELETE FROM TASK WHERE USER_ID = '{0}'", userId));
+				return "OK";
 			}
 			catch (Exception e)
 			{
