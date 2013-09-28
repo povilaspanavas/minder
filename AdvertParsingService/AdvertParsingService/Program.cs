7c25d1e310ecf557ceb5a75ef57d9e168dd4edb1
ign.bgd <ign.bgd@gmail.com>
2013-09-28 12:30:13 +0000

    Pradedu kodinti servisus skelbimu parsinimui

--- AdvertParsingService/AdvertParsingService/Program.cs
+++ AdvertParsingService/AdvertParsingService/Program.cs
@@ -14,12 +14,7 @@ namespace AdvertParsingService
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
 			
-			// TODO: Implement Functionality Here
-			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
 		}
 	}
 }
