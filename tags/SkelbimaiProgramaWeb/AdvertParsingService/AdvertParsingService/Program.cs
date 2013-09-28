7348ee47cba77941e272d7e289c59e7d4274af00
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2013-09-28 15:14:35 +0000

    Veikiantis prototipas su Autogidu

--- tags/SkelbimaiProgramaWeb/AdvertParsingService/AdvertParsingService/Program.cs
+++ tags/SkelbimaiProgramaWeb/AdvertParsingService/AdvertParsingService/Program.cs
@@ -7,6 +7,7 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using AdvertParsingService.Service;
 
 namespace AdvertParsingService
 {
@@ -14,7 +15,7 @@ namespace AdvertParsingService
 	{
 		public static void Main(string[] args)
 		{
-			
+			new AdvertService().Start();
 		}
 	}
 }
--- tags/SkelbimaiProgramaWeb/Pages/UserPanel/UserPanel.aspx.cs
+++ tags/SkelbimaiProgramaWeb/Pages/UserPanel/UserPanel.aspx.cs
@@ -9,6 +9,7 @@ using WebSite.Helpers;
 using Core.DB;
 using System.Data;
 using AdvertModel;
+using System.Text.RegularExpressions;
 
 public partial class Pages_UserPanel : System.Web.UI.Page
 {
@@ -20,15 +21,23 @@ public partial class Pages_UserPanel : System.Web.UI.Page
     int m_indexId = 0;
     int m_indexRead = 1;
     int m_indexSelectCheckBox = 2;
-    int m_indexUrl = 7;
+    int m_indexUrl = 8;
 
     protected void Page_Load(object sender, EventArgs e)
     {
         m_token = new TokenHelper().ValidateTokenOnFormOpen(this);
         m_userName = Request["username"].Replace("'", string.Empty);
+
         SetEvents();
         CheckTempPass();
         LoadGrid();
+
+
+        //Refresh kas 10 s.
+        Response.Expires = 0;
+        Response.Buffer = true;
+        Response.Clear();
+        Response.AddHeader("Refresh", "20");
     }
 
     private void SetEvents()
@@ -61,8 +70,8 @@ public partial class Pages_UserPanel : System.Web.UI.Page
                     int id = Convert.ToInt32(row.Cells[m_indexId].Text);
                     GenericDbHelper.RunDirectSql("update sp_advert set deleted = 1 where id = " + id);
                 }
-                Response.Redirect(string.Format("~/Pages/UserPanel/UserPanel.aspx?token='{0}'&username='{1}'", m_token.TokenValue, m_userName));
             }
+            Response.Redirect(string.Format("~/Pages/UserPanel/UserPanel.aspx?token='{0}'&username='{1}'", m_token.TokenValue, m_userName));
         };
 
         this.M_MarkAsReadButton.Click += delegate
@@ -80,6 +89,53 @@ public partial class Pages_UserPanel : System.Web.UI.Page
             }
             Response.Redirect(string.Format("~/Pages/UserPanel/UserPanel.aspx?token='{0}'&username='{1}'", m_token.TokenValue, m_userName));
         };
+
+        this.M_SelectAllButton.Click += delegate
+        {
+            foreach (GridViewRow row in M_AdvertGrid.Rows)
+            {
+                CheckBox checkBox = row.Cells[m_indexSelectCheckBox].Controls[0] as CheckBox;
+                checkBox.Checked = true;
+            }
+        };
+
+        this.M_RefreshButton.Click += delegate
+        {
+            Response.Redirect(string.Format("~/Pages/UserPanel/UserPanel.aspx?token='{0}'&username='{1}'", m_token.TokenValue, m_userName));
+        };
+
+        this.M_OpenLinkButton.Click += delegate
+        {
+            foreach (GridViewRow row in M_AdvertGrid.Rows)
+            {
+                CheckBox checkBox = row.Cells[m_indexSelectCheckBox].Controls[0] as CheckBox;
+                if (checkBox.Checked)
+                {
+                    string url = row.Cells[m_indexUrl].Text;
+                    Response.Write("<script>");
+                    Response.Write(string.Format("window.open('{0}','_blank')", url));
+                    Response.Write("</script>");
+                    if (row.Cells[m_indexRead].Text.Equals("False"))
+                        GenericDbHelper.RunDirectSql("update sp_advert set read = 1 where id = " + row.Cells[m_indexId].Text);
+                    checkBox.Checked = false;
+                }
+            }
+        };
+
+        //this.M_AdvertGrid.RowCommand += delegate(object sender, GridViewCommandEventArgs e)
+        //{
+        // if (e.CommandName.ToLower().Equals("openlink"))
+        //  {
+        //   string[] args = Regex.Split(e.CommandArgument.ToString(), ";separator;");
+        //   string url = args[0];
+
+        //        Response.Write("<script>");
+        //        Response.Write(string.Format("window.open('{0}','_blank')", url));
+        //        Response.Write("</script>");
+
+        //        GenericDbHelper.RunDirectSql("update sp_advert set read = 1 where id = "+args[1]);
+        //    }
+        ////};
     }
 
     private void CheckTempPass()
@@ -95,7 +151,9 @@ public partial class Pages_UserPanel : System.Web.UI.Page
             return;
 
         List<Advert> allAdverts = GenericDbHelper.Get<Advert>(string.Format("USER_ID = {0} and DELETED = 0", m_user.Id));
+        allAdverts = allAdverts.OrderByDescending(M => M.DateA).ToList();
         DataTable table = new GridViewHelper().ConvertObjectListToDataTable<Advert>(allAdverts);
+        table.Columns.RemoveAt(table.Columns.Count - 1);//Remove komandos column
         //Pridedamas checkboxas
         DataColumn coll = table.Columns.Add("Žymėti", Type.GetType("System.Boolean"));
         coll.SetOrdinal(m_indexSelectCheckBox);
