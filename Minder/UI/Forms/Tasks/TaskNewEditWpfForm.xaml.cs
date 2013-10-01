8ad132a2cd9984133cf935b18b7238b7f9e43731
povilas.panavas <povilas.panavas@gmail.com>
2013-10-01 22:19:06 +0000

    Added TaskNewEditWpfForm to change existing WinForms version

---
+++ Minder/UI/Forms/Tasks/TaskNewEditWpfForm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Minder.UI.Forms.Tasks
+{
+    /// <summary>
+    /// Interaction logic for TaskNewEditWpfForm.xaml
+    /// </summary>
+    public partial class TaskNewEditWpfForm : BaseWpfForm
+    {
+        public TaskNewEditWpfForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
