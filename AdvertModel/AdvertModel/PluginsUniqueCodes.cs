364cf0ff7a18606ee060e81edd3c8755e854032b
ign.bgd <ign.bgd@gmail.com>
2013-10-10 16:22:38 +0000

    Trukstami failiukai

---
+++ AdvertModel/AdvertModel/PluginsUniqueCodes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdvertModel
+{
+    public static class PluginsUniqueCodes
+    {
+        public static string[] PluginsUniquecodes = new string[] 
+        { "Autogidas.lt", 
+          "Autoplius.lt" 
+        };
+    }
+}
---
+++ Pages/UserPanel/ProfilePage.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebSite.Helpers;
+using WebSite.Model;
+
+public partial class Pages_UserPanel_ProfilePage : System.Web.UI.Page
+{
+    private Token m_token = null;
+    private string m_userName;
+    private User m_user;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        m_token = new TokenHelper().ValidateTokenOnFormOpen(this);
+        m_userName = Request["username"].Replace("'", string.Empty);
+    }
+}
