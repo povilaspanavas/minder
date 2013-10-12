fbf9c7f24f4d7a0462804fa70a0d9a954f62534b
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2013-10-12 13:22:05 +0000

    Daromas vartotojų slaptažodžio atstatymas.

--- tags/SkelbimaiProgramaWeb/App_Code/Helpers/RestorePasswordHelper.cs
+++ tags/SkelbimaiProgramaWeb/App_Code/Helpers/RestorePasswordHelper.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Core.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebSite.Model;
 
 namespace WebSite.Helpers
 {
@@ -16,7 +18,19 @@ namespace WebSite.Helpers
         /// </summary>
         public string TryRestore(string mail)
         {
-            return string.Empty;
+            if (string.IsNullOrEmpty(mail))
+                return "Nenurodytas el. paštas!";
+            if (mail.IndexOf("@") == -1)
+                return "Neteisingas el. paštas!";
+            List<User> user = GenericDbHelper.Get<User>(string.Format("EMAIL = '{0}'", mail));
+            if (user == null || user.Count == 0)
+                return "Su tokiu el. pašto adresu nerastas nei vienas naudotojas!";
+            else if(user.Count == 1)
+            {
+                //TODO IGNO padaryti nuorodas restorinimui
+                return "Informacija išsiųsta į nurodytą el. paštą!";
+            }
+            return "Nenustatyta klaida!";
         }
     }
 }
