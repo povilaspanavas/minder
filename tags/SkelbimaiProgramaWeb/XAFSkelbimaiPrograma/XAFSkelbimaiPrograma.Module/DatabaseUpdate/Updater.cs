c584bd6fc6a871a64cfff3fbdd834c0b31e20173
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2013-10-18 13:57:44 +0000

    Pridėti skelbimai į navigate

--- tags/SkelbimaiProgramaWeb/XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Module/DatabaseUpdate/Updater.cs
+++ tags/SkelbimaiProgramaWeb/XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Module/DatabaseUpdate/Updater.cs
@@ -29,56 +29,8 @@ namespace XAFSkelbimaiPrograma.Module.DatabaseUpdate
         public override void UpdateDatabaseAfterUpdateSchema()
         {
             base.UpdateDatabaseAfterUpdateSchema();
+            CreateDefaultUsers();
 
-            DLCEmployeeRole adminRole = ObjectSpace.FindObject<DLCEmployeeRole>(
-       new BinaryOperator("Name", SecurityStrategy.AdministratorRoleName));
-            if (adminRole == null)
-            {
-                adminRole = ObjectSpace.CreateObject<DLCEmployeeRole>();
-                adminRole.Name = SecurityStrategy.AdministratorRoleName;
-                adminRole.IsAdministrative = true;
-            }
-
-            DLCEmployeeRole userRole = ObjectSpace.FindObject<DLCEmployeeRole>(
-        new BinaryOperator("Name", "User"));
-            if (userRole == null)
-            {
-                userRole = ObjectSpace.CreateObject<DLCEmployeeRole>();
-                userRole.Name = "User";
-                SecuritySystemTypePermissionObject userTypePermission =
-                    ObjectSpace.CreateObject<SecuritySystemTypePermissionObject>();
-                userTypePermission.TargetType = typeof(SecuritySystemUser);
-                SecuritySystemObjectPermissionsObject currentUserObjectPermission =
-                    ObjectSpace.CreateObject<SecuritySystemObjectPermissionsObject>();
-                currentUserObjectPermission.Criteria = "[Oid] = CurrentUserId()";
-                currentUserObjectPermission.AllowNavigate = true;
-                currentUserObjectPermission.AllowRead = true;
-                userTypePermission.ObjectPermissions.Add(currentUserObjectPermission);
-                userRole.TypePermissions.Add(userTypePermission);
-            }
-
-
-            DLCEmployee user1 = ObjectSpace.FindObject<DLCEmployee>(
-       new BinaryOperator("UserName", "Sam"));
-            if (user1 == null){
-                user1 = ObjectSpace.CreateObject<DLCEmployee>();
-                user1.UserName = "Sam";
-                // Set a password if the standard authentication type is used 
-                user1.SetPassword("");
-            }
-            // If a user named 'John' doesn't exist in the database, create this user 
-            DLCEmployee user2 = ObjectSpace.FindObject<DLCEmployee>(
-                 new BinaryOperator("UserName", "John"));
-            if (user2 == null){
-                user2 = ObjectSpace.CreateObject<DLCEmployee>();
-                user2.UserName = "John";
-                // Set a password if the standard authentication type is used 
-                user2.SetPassword("");
-
-
-            }
-            adminRole.Employees.Add(user1);
-            adminRole.Employees.Add(user2);
             //string name = "MyName";
             //DomainObject1 theObject = ObjectSpace.FindObject<DomainObject1>(CriteriaOperator.Parse("Name=?", name));
             //if(theObject == null) {
@@ -92,6 +44,61 @@ namespace XAFSkelbimaiPrograma.Module.DatabaseUpdate
             //if(CurrentDBVersion < new Version("1.1.0.0") && CurrentDBVersion > new Version("0.0.0.0")) {
             //    RenameColumn("DomainObject1Table", "OldColumnName", "NewColumnName");
             //}
+        }
+
+        private void CreateDefaultUsers()
+        {
+            DLCEmployeeRole adminRole = ObjectSpace.FindObject<DLCEmployeeRole>(
+      new BinaryOperator("Name", SecurityStrategy.AdministratorRoleName));
+            if (adminRole == null)
+            {
+                adminRole = ObjectSpace.CreateObject<DLCEmployeeRole>();
+                adminRole.Name = SecurityStrategy.AdministratorRoleName;
+                adminRole.IsAdministrative = true;
+            }
+
+            //    DLCEmployeeRole userRole = ObjectSpace.FindObject<DLCEmployeeRole>(
+            //new BinaryOperator("Name", "User"));
+            //    if (userRole == null)
+            //    {
+            //        userRole = ObjectSpace.CreateObject<DLCEmployeeRole>();
+            //        userRole.Name = "User";
+            //        SecuritySystemTypePermissionObject userTypePermission =
+            //            ObjectSpace.CreateObject<SecuritySystemTypePermissionObject>();
+            //        userTypePermission.TargetType = typeof(SecuritySystemUser);
+            //        SecuritySystemObjectPermissionsObject currentUserObjectPermission =
+            //            ObjectSpace.CreateObject<SecuritySystemObjectPermissionsObject>();
+            //        currentUserObjectPermission.Criteria = "[Oid] = CurrentUserId()";
+            //        currentUserObjectPermission.AllowNavigate = true;
+            //        currentUserObjectPermission.AllowRead = true;
+            //        userTypePermission.ObjectPermissions.Add(currentUserObjectPermission);
+            //        userRole.TypePermissions.Add(userTypePermission);
+            //    }
+
+
+            DLCEmployee user1 = ObjectSpace.FindObject<DLCEmployee>(
+                    new BinaryOperator("UserName", "Admin"));
+            if (user1 == null)
+            {
+                user1 = ObjectSpace.CreateObject<DLCEmployee>();
+                user1.UserName = "Admin";
+                // Set a password if the standard authentication type is used 
+                user1.SetPassword("masterkey");
+            }
+            // If a user named 'John' doesn't exist in the database, create this user 
+            //DLCEmployee user2 = ObjectSpace.FindObject<DLCEmployee>(
+            //     new BinaryOperator("UserName", "John"));
+            //if (user2 == null)
+            //{
+            //    user2 = ObjectSpace.CreateObject<DLCEmployee>();
+            //    user2.UserName = "John";
+            //    // Set a password if the standard authentication type is used 
+            //    user2.SetPassword("");
+
+
+            //}
+            adminRole.Employees.Add(user1);
+            //adminRole.Employees.Add(user2);
         }
     }
 }
