9057efc569dcc7bd1298959c4250360ba0b782c2
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2013-10-18 18:30:57 +0000

    Pridedu trukstamus failus

---
+++ tags/SkelbimaiProgramaWeb/XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Module/BusinessObjects/ORMDataModelCode/Classificators/SKPlugin.bo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XAFSkelbimaiPrograma.Module.BusinessObjects.ORMDataModelCode.Classificators
+{
+    public partial class SKPlugin
+    {
+    }
+}
