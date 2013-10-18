daf0f74ec649e33dcc3a1b74f3662479e40cd2ae
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2013-10-18 13:24:45 +0000

    sukomitinu pakeitimus

--- tags/SkelbimaiProgramaWeb/XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Module/DatabaseUpdate/Updater.cs
+++ tags/SkelbimaiProgramaWeb/XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Module/DatabaseUpdate/Updater.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using DevExpress.ExpressApp.Security.Strategy;
 using DevExpress.Xpo;
 using DevExpress.ExpressApp;
 using DevExpress.Data.Filtering;
@@ -8,10 +9,13 @@ using DevExpress.Persistent.Base;
 using DevExpress.ExpressApp.Updating;
 using DevExpress.Persistent.BaseImpl;
 using DevExpress.ExpressApp.Security;
+using PUVWeb_TEST.Module.DLCSecuritySystem;
+using XAFSkelbimaiPrograma.Module.BusinessObjects.ORMDataModelCode.Objects;
 //using DevExpress.ExpressApp.Reports;
 //using DevExpress.ExpressApp.PivotChart;
 //using DevExpress.ExpressApp.Security.Strategy;
 //using XAFSkelbimaiPrograma.Module.BusinessObjects;
+using XAFSkelbimaiPrograma.Module.SecurityModule;
 
 namespace XAFSkelbimaiPrograma.Module.DatabaseUpdate
 {
@@ -25,6 +29,56 @@ namespace XAFSkelbimaiPrograma.Module.DatabaseUpdate
         public override void UpdateDatabaseAfterUpdateSchema()
         {
             base.UpdateDatabaseAfterUpdateSchema();
+
+            DLCEmployeeRole adminRole = ObjectSpace.FindObject<DLCEmployeeRole>(
+       new BinaryOperator("Name", SecurityStrategy.AdministratorRoleName));
+            if (adminRole == null)
+            {
+                adminRole = ObjectSpace.CreateObject<DLCEmployeeRole>();
+                adminRole.Name = SecurityStrategy.AdministratorRoleName;
+                adminRole.IsAdministrative = true;
+            }
+
+            DLCEmployeeRole userRole = ObjectSpace.FindObject<DLCEmployeeRole>(
+        new BinaryOperator("Name", "User"));
+            if (userRole == null)
+            {
+                userRole = ObjectSpace.CreateObject<DLCEmployeeRole>();
+                userRole.Name = "User";
+                SecuritySystemTypePermissionObject userTypePermission =
+                    ObjectSpace.CreateObject<SecuritySystemTypePermissionObject>();
+                userTypePermission.TargetType = typeof(SecuritySystemUser);
+                SecuritySystemObjectPermissionsObject currentUserObjectPermission =
+                    ObjectSpace.CreateObject<SecuritySystemObjectPermissionsObject>();
+                currentUserObjectPermission.Criteria = "[Oid] = CurrentUserId()";
+                currentUserObjectPermission.AllowNavigate = true;
+                currentUserObjectPermission.AllowRead = true;
+                userTypePermission.ObjectPermissions.Add(currentUserObjectPermission);
+                userRole.TypePermissions.Add(userTypePermission);
+            }
+
+
+            DLCEmployee user1 = ObjectSpace.FindObject<DLCEmployee>(
+       new BinaryOperator("UserName", "Sam"));
+            if (user1 == null){
+                user1 = ObjectSpace.CreateObject<DLCEmployee>();
+                user1.UserName = "Sam";
+                // Set a password if the standard authentication type is used 
+                user1.SetPassword("");
+            }
+            // If a user named 'John' doesn't exist in the database, create this user 
+            DLCEmployee user2 = ObjectSpace.FindObject<DLCEmployee>(
+                 new BinaryOperator("UserName", "John"));
+            if (user2 == null){
+                user2 = ObjectSpace.CreateObject<DLCEmployee>();
+                user2.UserName = "John";
+                // Set a password if the standard authentication type is used 
+                user2.SetPassword("");
+
+
+            }
+            adminRole.Employees.Add(user1);
+            adminRole.Employees.Add(user2);
             //string name = "MyName";
             //DomainObject1 theObject = ObjectSpace.FindObject<DomainObject1>(CriteriaOperator.Parse("Name=?", name));
             //if(theObject == null) {
