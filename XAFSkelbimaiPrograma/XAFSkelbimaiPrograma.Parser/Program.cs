08a929e542e7ee41d084fa4c59bd5e4b70ba0aa0
ign.bgd <ign.bgd@gmail.com>
2013-10-19 12:27:18 +0000

    Pridėtas xpo modulis į parserį

--- XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Parser/Program.cs
+++ XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Parser/Program.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp.Core;
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using XAFSkelbimaiPrograma.Module.SecurityModule;
 
 namespace XAFSkelbimaiPrograma.Parser
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
+            UnitOfWork session2 = new UnitOfWork();
+            session2.ConnectionString = "XpoProvider = Firebird; DataSource = localhost; User = sysdba; Password = masterkey; Database = XAF_DB; ServerType = 0; Charset = UTF8";
+            var me = session2.FindObject<DLCEmployee>(CriteriaOperator.Parse("FullName = 'Ignas Bagdonas'"));
             Console.ReadKey(true);
         }
+
+       
     }
 }
