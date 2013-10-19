940eaa519092f8f397e767dbc348e422f82c332f
ign.bgd <ign.bgd@gmail.com>
2013-10-19 13:20:39 +0000

    Parserio programavimas

---
+++ XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Module/BusinessObjects/ORMDataModelCode/StaticData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XAFSkelbimaiPrograma.Module.BusinessObjects.ORMDataModelCode
+{
+    public static class StaticData
+    {
+        public const string CONNECTION_STRING = "XpoProvider = Firebird; DataSource = localhost; User = sysdba; Password = masterkey; Database = XAF_DB; ServerType = 0; Charset = UTF8";
+    }
+}
--- XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Parser/Program.cs
+++ XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Parser/Program.cs
@@ -1,25 +1,38 @@
 ﻿using System;
+//using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DevExpress.Data.Filtering;
-using DevExpress.ExpressApp.Core;
-using DevExpress.Xpo;
-using DevExpress.Xpo.DB;
-using XAFSkelbimaiPrograma.Module.SecurityModule;
+//using DevExpress.Data.Filtering;
+//using DevExpress.Xpo;
+//using DevExpress.Xpo.Metadata;
+//using XAFSkelbimaiPrograma.Module;
+//using XAFSkelbimaiPrograma.Module.BusinessObjects.ORMDataModelCode.Objects;
 
 namespace XAFSkelbimaiPrograma.Parser
 {
     class Program
     {
-        static void Main(string[] args){
-            UnitOfWork session2 = new UnitOfWork();
-            session2.ConnectionString = "XpoProvider = Firebird; DataSource = localhost; User = sysdba; Password = masterkey; Database = XAF_DB; ServerType = 0; Charset = UTF8";
-            var me = session2.FindObject<DLCEmployee>(CriteriaOperator.Parse("FullName = 'Ignas Bagdonas'"));
+        static void Main(string[] args)
+        {
+            //TryLoadAllSettings();
             Console.ReadKey(true);
         }
 
-       
+        //private static void TryLoadAllSettings()
+        //{
+        //    ICollection settings;
+        //    XPClassInfo settingsClass;
+
+        //    Session session = new Session() { ConnectionString = StaticData.CONNECTION_STRING };
+        //    settingsClass = session.GetClassInfo(typeof(SKUserSearchSettings));
+
+        //    settings = session.GetObjects(settingsClass, null, null, 0, null, true);
+
+
+        //    //var me = session2.FindObject<DLCEmployee>(CriteriaOperator.Parse("FullName = 'Ignas Bagdonas'"));
+
+        //}
+
+
     }
 }
