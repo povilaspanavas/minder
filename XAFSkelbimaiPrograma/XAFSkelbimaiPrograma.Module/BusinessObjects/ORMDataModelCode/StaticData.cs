8fdf7952a0dd1485345f29546c73ab7aa87acedb
ign.bgd <ign.bgd@gmail.com>
2013-10-19 14:29:58 +0000

    Parserio programavimas

--- XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Module/BusinessObjects/ORMDataModelCode/StaticData.cs
+++ XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Module/BusinessObjects/ORMDataModelCode/StaticData.cs
@@ -8,5 +8,25 @@ namespace XAFSkelbimaiPrograma.Module.BusinessObjects.ORMDataModelCode
     public static class StaticData
     {
         public const string CONNECTION_STRING = "XpoProvider = Firebird; DataSource = localhost; User = sysdba; Password = masterkey; Database = XAF_DB; ServerType = 0; Charset = UTF8";
+
+        public static string[] PluginsUniqueCodes = new string[] 
+        { 
+          "Autogidas.lt", 
+          "Autoplius.lt",
+          "Plius.lt", 
+          "Skelbiu.lt",
+          "Auto24.ee",
+          "Blocket.se",
+          "Mobile.de",
+          "Marktplaats.nl",
+          "Finn.no",
+          "Gaspedaal.nl",
+          "Speurders.nl",
+          "Otomoto.pl",
+          "Allegro.pl",
+          "Leboncoin.fr",
+          "Donedeal.ie",
+          "Autotrader.co.uk"
+        };
     }
 }
