b6a105adc4414ce4f27b911f7a0ebbb80c311cd9
ign.bgd <ign.bgd@gmail.com>
2013-10-26 13:49:49 +0000

    Perkelinėjami pluginai

---
+++ XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Parser/Plugins/PluginAllegro.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using XAFSkelbimaiPrograma.Parser.Helpers;
+
+namespace XAFSkelbimaiPrograma.Parser.Plugins
+{
+    public class PluginAllegro : IPlugin
+    {
+
+        public List<AdvertDto> Parse(string url)
+        {
+            string source = new SourceHelper().GetSource(url);
+            List<string> parts = ParseToParts(source);
+            return ParseToAdvertDtos(parts);
+        }
+
+        private List<AdvertDto> ParseToAdvertDtos(List<string> parts)
+        {
+            List<AdvertDto> result = new List<AdvertDto>();
+
+            foreach (string part in parts)
+            {
+                AdvertDto advert = new AdvertDto();
+                advert.Name = GetName(part);
+                advert.UrlLink = GetLink(part);
+                advert.Price = GetPrice(part);
+                //advert.Column4 = GetExpiry(part);
+                advert.Year = GetYear(part);
+
+                //advert.Image = GetImage(part);
+
+                result.Add(advert);
+            }
+
+            return result;
+        }
+
+        #region fields func
+        private string GetName(string part)
+        {
+            string[] parts = Regex.Split(part, "<h2>");
+            if (parts.Length <= 1)
+                return string.Empty;
+            string[] parts2 = Regex.Split(parts[1], "<span>");
+            string[] parts3 = Regex.Split(parts2[1], "</span>");
+            return parts3[0];
+        }
+
+        private string GetLink(string part)
+        {
+            string[] parts = Regex.Split(part, "<h2>");
+            if (parts.Length <= 1)
+                return string.Empty;
+            string[] parts2 = Regex.Split(parts[1], "<a href=\"");
+            string[] parts3 = Regex.Split(parts2[1], "\">");
+            return "http://allegro.pl" + parts3[0];
+        }
+
+        private string GetPrice(string part)
+        {
+            string[] parts = Regex.Split(part, "buy-now dist\">");
+            if (parts.Length <= 1)
+                parts = Regex.Split(part, "price\">");
+            string[] parts2 = Regex.Split(parts[1], "</span>");
+            string[] parts3 = Regex.Split(parts2[1], "<span");
+            return parts3[0].Trim();
+        }
+
+        //private string GetExpiry(string part)
+        //{
+        //    string[] parts = Regex.Split(part, "expiry\">");
+        //    if (parts.Length <= 1)
+        //        return string.Empty;
+        //    string[] parts2 = Regex.Split(parts[1], ">");
+        //    string[] parts3 = Regex.Split(parts2[1], "</strong");
+        //    return parts3[0].Trim();
+        //}
+
+        //private string GetImage(string part)
+        //{
+        //    if (ImageHelper.LoadImages() == false)
+        //        return string.Empty;
+
+        //    string[] parts = Regex.Split(part, "data-img='");
+        //    if (parts.Length <= 1)
+        //        return string.Empty;
+        //    string[] parts2 = Regex.Split(parts[1].Replace("[[\"", string.Empty), "\"");
+        //    return ImageHelper.ConvertToBase64(parts2[0].Trim());
+        //}
+
+        private string GetYear(string part)
+        {
+            string[] parts = Regex.Split(part, "Rok produkcji");
+            if (parts.Length <= 1)
+                return string.Empty;
+            string[] parts2 = Regex.Split(parts[1], "</span></dd>");
+            string result = parts2[0].Replace("</span></dt><dd><span>", string.Empty);
+            return result.Trim();
+        }
+
+        #endregion
+
+        private List<string> ParseToParts(string source)
+        {
+            List<string> parts = Regex.Split(source, "<article id=\"").ToList();
+            if (parts.Count > 0)
+                parts.RemoveAt(0);
+
+            return parts;
+        }
+
+        public string UniqueCode
+        {
+            get { return "Allegro.pl"; }
+        }
+
+        public List<string> TestLinks
+        {
+            get
+            {
+                return new List<string>(new string[] 
+                {
+                    "http://allegro.pl/listing/listing.php?category=1454&sg=0&order=qd&string=Zara&change_view=1",
+                    "http://allegro.pl/ogrod-meble-ogrodowe-82256?order=qd&string=",
+                    "http://allegro.pl/samochody-149?a_text_i%5B1%5D%5B0%5D=2008&a_text_i%5B1%5D%5B1%5D=2010&a_text_i%5B4%5D%5B0%5D=&a_text_i%5B4%5D%5B1%5D=&a_text_i%5B5%5D%5B0%5D=&a_text_i%5B5%5D%5B1%5D=&a_text_i_enabled%5B1%5D%5B0%5D=1&a_text_i_enabled%5B1%5D%5B1%5D=1&change_view=Poka%C5%BC%C2%A0%3E&distance=1&limit=180&listing=1&offerTypeAdvert=0&offerTypeAuction=0&offerTypeBuyNow=0&order=p&price_enabled=1&price_from=5000&price_to=30000&startingTime=1&startingTime_enabled=1&state=0"
+                }
+                    );
+            }
+        }
+    }
+}
---
+++ XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Parser/Plugins/PluginAuto24.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using XAFSkelbimaiPrograma.Parser.Helpers;
+
+namespace XAFSkelbimaiPrograma.Parser.Plugins
+{
+    public class PluginAuto24 : IPlugin
+    {
+
+        public List<AdvertDto> Parse(string url)
+        {
+            string source = new SourceHelper().GetSource(url);
+            return ParseSource(source);
+        }
+
+        private List<AdvertDto> ParseSource(string source)
+        {
+            List<string> parts = SplitToParts(source);
+            List<AdvertDto> result = new List<AdvertDto>();
+
+            foreach (string part in parts)
+            {
+                if (part.IndexOf("SEB Liising") != -1)
+                    continue;
+
+                AdvertDto advert = new AdvertDto();
+                advert.Name = GetName(part);
+
+                advert.Year = GetYear(part);
+                advert.Price = GetPrice(part);
+                advert.UrlLink = GetLink(part);
+
+                result.Add(advert);
+            }
+
+
+            return result;
+        }
+
+        private string GetName(string part)
+        {
+            string[] parts = Regex.Split(part, "<a href=\\\"");
+            string[] parts2 = Regex.Split(parts[1], "\">");
+            string[] parts3 = Regex.Split(parts2[1], "</a>");
+
+            return parts3[0].Trim();
+        }
+
+        private string GetLink(string part)
+        {
+            string[] parts = Regex.Split(part, "<a href=\\\"");
+            string[] parts2 = Regex.Split(parts[1], "\">");
+
+            return string.Format("http://www.auto24.ee{0}", parts2[0].Trim());
+        }
+
+        private string GetPrice(string part)
+        {
+            string[] parts = Regex.Split(part, "class=\"price\">");
+            if (parts.Length < 1)
+                return string.Empty;
+            string[] parts2 = Regex.Split(parts[1], "</td>");
+
+            string result = string.Empty;
+
+            foreach (char r in parts2[0].Trim())
+            {
+                try
+                {
+                    int tempInt = Convert.ToInt32(r.ToString());
+                    result += r.ToString();
+                }
+                catch
+                {
+                }
+
+            }
+
+            return result;
+        }
+
+        private string GetYear(string part)
+        {
+            try
+            {
+                string[] parts = Regex.Split(part, "class=\"year\">");
+                if (parts.Length < 1)
+                    return string.Empty;
+                string[] parts2 = Regex.Split(parts[1], "</td>");
+
+                return parts2[0];
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        //[Obsolete("Neaišku kas čia ir ar išvis reikia")]
+        //private string GetFuelType(string part)
+        //{
+        //    string[] parts = Regex.Split(part, "class=\"fuel\">");
+        //    if (parts.Length < 1)
+        //        return string.Empty;
+        //    string[] parts2 = Regex.Split(parts[1], "</td>");
+
+        //    return parts2[0];
+        //}
+
+        private List<string> SplitToParts(string source)
+        {
+            string[] parts = Regex.Split(source, "block");
+            List<string> result = new List<string>(parts);
+            result.RemoveAt(0);
+            result.RemoveAt(0);
+            return result;
+        }
+
+
+        public string UniqueCode
+        {
+            get { return "Auto24.ee"; }
+        }
+
+        public List<string> TestLinks
+        {
+            get { return new List<string>(new string[] 
+            {
+                "http://www.auto24.ee/kasutatud/nimekiri.php?bn=2&bi=EUR&ab=0&ae=2&af=50&ag=1&otsi=otsi",
+                "http://rus.auto24.ee/kasutatud/nimekiri.php?bn=2&a=100&aj=&a=0&j=0&b=1&bw=165&c=&b2=0&bw2=0&c2=&b3=0&bw3=0&c3=&f1=1998&f2=&g1=&g2=&bi=EUR&k1=&k2=&l1=&l2=&h=0&i=0&p=0&m=0&n=0&ab=0&ac=0&ad=0&ae=2&af=50&by=0&ag=0&ag=1&otsi=%D0%BF%D0%BE%D0%B8%D1%81%D0%BA",
+                "http://rus.auto24.ee/kasutatud/nimekiri.php?bn=2&a=100&aj=&a=0&j=0&b=0&bw=0&c=&b2=0&bw2=0&c2=&b3=0&bw3=0&c3=&f1=2001&f2=&g1=&g2=&bi=EUR&k1=&k2=&l1=&l2=&h=0&i=0&p=0&m=0&n=0&ab=0&ac=0&ad=1&ae=2&af=50&by=0&ag=0&ag=1&otsi=%D0%BF%D0%BE%D0%B8%D1%81%D0%BA"
+            }); }
+        }
+    }
+}
