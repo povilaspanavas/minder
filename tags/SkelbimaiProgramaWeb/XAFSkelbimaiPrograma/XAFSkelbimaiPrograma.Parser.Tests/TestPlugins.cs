f11f2c674cdb4dc618f3a68c871565d89e19a7f4
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2013-10-26 11:54:27 +0000

    Pakeistas frameworkas ir pridėti testai

---
+++ tags/SkelbimaiProgramaWeb/XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Parser.Tests/TestPlugins.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XAFSkelbimaiPrograma.Parser.Tests
+{
+    [TestClassAttribute]
+    public class TestPlugins
+    {
+        [TestMethod]
+        public void TestAllPlugins()
+        {
+            Assert.AreEqual(0, 1);
+        }
+
+    }
+}
