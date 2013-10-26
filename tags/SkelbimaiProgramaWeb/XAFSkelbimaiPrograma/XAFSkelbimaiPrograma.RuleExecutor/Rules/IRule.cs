bf1c8a38575251396ef44bf48a08e0860bb59bdb
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2013-10-26 22:51:19 +0000

    Pridėtas taisyklių executorius

---
+++ tags/SkelbimaiProgramaWeb/XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.RuleExecutor/Rules/IRule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XAFSkelbimaiPrograma.RuleExecutor.Rules
+{
+    interface IRule
+    {
+        void Execute();
+    }
+}
