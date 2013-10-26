8c996d76b1d38bdcbabb027a1da3d6f16e4d86c4
ign.bgd <ign.bgd@gmail.com>
2013-10-26 22:51:19 +0000

    Pridėtas taisyklių executorius

---
+++ XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.RuleExecutor/Executor/RuleExecutor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using XAFSkelbimaiPrograma.RuleExecutor.Rules;
+
+namespace XAFSkelbimaiPrograma.RuleExecutor.Executor
+{
+    class RuleExecutor
+    {
+        public void RunRules()
+        {
+            var instances = from t in Assembly.GetExecutingAssembly().GetTypes()
+                            where t.GetInterfaces().Contains(typeof(IRule))
+                                     && t.GetConstructor(Type.EmptyTypes) != null
+                            select Activator.CreateInstance(t) as IRule;
+
+            foreach (IRule rule in instances)
+            {
+                rule.Execute();
+            }
+        }
+    }
+}
