4cae0ff6a105b1387850bc72fefaf0e0b36c78d8
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2013-10-28 16:08:43 +0000

    Padarytas log objektas

---
+++ tags/SkelbimaiProgramaWeb/XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Module/BusinessObjects/ORMDataModelCode/Objects/SKParserLog.bo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XAFSkelbimaiPrograma.Module.BusinessObjects.ORMDataModelCode.Objects
+{
+    public partial class SKParserLog
+    {
+    }
+}
