bfe20a7597d485eb649355c3603b2a94bf73352e
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2013-10-28 17:43:12 +0000

    Pridėtas parserio starteris

---
+++ tags/SkelbimaiProgramaWeb/XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Parser.Starter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XAFSkelbimaiPrograma.Parser.Starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            while (true)
+            {
+                Process[] proc = Process.GetProcessesByName("XAFSkelbimaiPrograma.Parser");
+                if (proc.Length == 0)
+                    Process.Start(@"..\..\..\XAFSkelbimaiPrograma.Parser\bin\debug\XAFSkelbimaiPrograma.Parser.exe");
+                Thread.Sleep(1000 * 5);
+            }
+        }
+    }
+}
