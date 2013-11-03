4aa076c9ef16d2e01f0c1323c550afcb9d2a0a07
ign.bgd <ign.bgd@gmail.com>
2013-11-03 18:14:22 +0000

    rule executor

---
+++ XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.RuleExecutor/Rules/RuleBlockLicence.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XAFSkelbimaiPrograma.RuleExecutor.Rules
+{
+    public class RuleBlockLicence : IRule
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
