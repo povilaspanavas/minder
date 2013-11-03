039194bfedc690301eeefe1f2568d8a0d52682d4
ign.bgd <ign.bgd@gmail.com>
2013-11-03 14:40:37 +0000

    Padarytas ribojimas skelbimų

---
+++ XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Module/Controllers/AdvertCountErrorController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Linq;
+using System.Text;
+using DevExpress.ExpressApp;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.Persistent.Validation;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+
+namespace XAFSkelbimaiPrograma.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out http://documentation.devexpress.com/#Xaf/clsDevExpressExpressAppViewControllertopic.
+    public partial class AdvertCountErrorController : ViewController
+    {
+        public AdvertCountErrorController()
+        {
+            InitializeComponent();
+            RegisterActions(components);
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+            this.TargetViewId = "SKAdvertRead_ListView";
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+            ListView listView = View as ListView;
+            int count = listView.CollectionSource.GetCount();
+            if (count > 300)
+                MessageBox.Show(Application, "Turimø skelbimø kiekis jau didesnis nei 300! Praðome iðtrinti perskaitytus skelbimus.",
+                       new Action(delegate
+                       {
+
+                       }),
+                       new Action(delegate
+                       {
+
+                       }));
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
