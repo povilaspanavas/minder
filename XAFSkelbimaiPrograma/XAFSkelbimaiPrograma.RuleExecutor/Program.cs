2e0801d66c23a4d479c03429653ba8a218b9edae
ign.bgd <ign.bgd@gmail.com>
2013-11-08 18:17:45 +0000

    Padarytas taisyklių vykdymas

--- XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.RuleExecutor/Program.cs
+++ XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.RuleExecutor/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace XAFSkelbimaiPrograma.RuleExecutor
@@ -10,7 +11,13 @@ namespace XAFSkelbimaiPrograma.RuleExecutor
     {
         static void Main(string[] args)
         {
-            new RuleExecutor.Executor.RuleExecutor().RunRules();
+            while (true)
+            {
+                Console.WriteLine("Execute rules...");
+                new RuleExecutor.Executor.RuleExecutor().RunRules();
+                Console.WriteLine("Rules executed.");
+                Thread.Sleep(60 * 1000 * 50);
+            }
         }
     }
 }
