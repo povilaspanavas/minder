ae243eadd90676e6972c5a5d7c9c48a52ca0bc34
ign.bgd <ign.bgd@gmail.com>
2013-11-11 16:47:33 +0000

    Sound eventas artui

---
+++ XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Module/Controllers/SoundViewController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Linq;
+using System.Text;
+using DevExpress.ExpressApp;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.Persistent.Validation;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+
+namespace XAFSkelbimaiPrograma.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out http://documentation.devexpress.com/#Xaf/clsDevExpressExpressAppViewControllertopic.
+    public partial class SoundViewController : ViewController
+    {
+        public SoundViewController()
+        {
+            InitializeComponent();
+            RegisterActions(components);
+            this.TargetViewId = "SKAdvertNew_ListView";
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+            ListView view = this.View as ListView;
+            int count = view.CollectionSource.GetCount();
+            if (count != 0)
+            {
+                //Beep this
+            }
+
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
