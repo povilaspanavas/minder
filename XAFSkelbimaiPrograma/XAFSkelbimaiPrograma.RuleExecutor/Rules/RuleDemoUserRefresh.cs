60a9dbede34384e7ab2af2caa3529b41758e7d32
ign.bgd <ign.bgd@gmail.com>
2013-11-11 17:08:17 +0000

    Padarytas demo userio valymas kas valanda

---
+++ XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.RuleExecutor/Rules/RuleDemoUserRefresh.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Data.Filtering;
+using DevExpress.Xpo;
+using XAFSkelbimaiPrograma.Module.BusinessObjects.ORMDataModelCode;
+using XAFSkelbimaiPrograma.Module.SecurityModule;
+
+namespace XAFSkelbimaiPrograma.RuleExecutor.Rules
+{
+    public class RuleDemoUserRefresh : IRule
+    {
+        public void Execute()
+        {
+            Session session = new Session { ConnectionString = StaticData.CONNECTION_STRING };
+            DLCEmployee demoUser = session.FindObject<DLCEmployee>(CriteriaOperator.Parse("UserName = ?", "demo"));
+            session.ExecuteNonQuery(string.Format("delete from \"SKAdvert\" where \"SKUser\" = '{0}'", demoUser.Oid));
+            session.ExecuteNonQuery(string.Format("delete from \"SKUserSearchSettings\" where \"SKUser\" = '{0}'", demoUser.Oid));
+        }
+    }
+}
