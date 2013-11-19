48a30f0e8502f4d7a63c07b212b60456b5e47e82
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2013-11-19 21:03:50 +0000

    Proxy padarytas, bet dar reikia normaliai pratestuot

--- tags/SkelbimaiProgramaWeb/XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Parser/Helpers/SourceHelper.cs
+++ tags/SkelbimaiProgramaWeb/XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Parser/Helpers/SourceHelper.cs
@@ -10,13 +10,50 @@ namespace XAFSkelbimaiPrograma.Parser.Helpers
 {
     public class SourceHelper
     {
+        private List<string> m_proxies;
+
         public string GetSource(string link) //TODO Need implement proxy
         {
-            WebClient webClient = new WebClient();
-            //webClient.Headers["Accept-Language"] = "lt-LT";
-            string source = webClient.DownloadString(link);
-            webClient.Dispose();
-            return source;
+            //WebClient webClient = new WebClient();
+            ////webClient.Headers["Accept-Language"] = "lt-LT";
+            //string source = webClient.DownloadString(link);
+            //webClient.Dispose();
+            //return source;
+
+            InitProxyList();
+            WebClient client = new WebClient();
+            WebProxy wp = new WebProxy(GetRandomProxy());
+            client.Proxy = wp;
+            string str = client.DownloadString(link);
+            client.Dispose();
+            return str;
+
+        }
+
+        private string GetRandomProxy()
+        {
+            lock (m_proxies)
+            {
+                int start = 0;
+                int end = m_proxies.Count - 1;
+                int ticks = (int)DateTime.Now.Ticks; 
+                Random random = new Random(ticks);
+                int randomInt = random.Next(start, end);
+                return m_proxies[randomInt];
+            }
+        }
+
+        private void InitProxyList()
+        {
+            if (m_proxies != null)
+                return;
+
+            m_proxies = new List<string>();
+            lock (m_proxies)
+            {
+                string[] proxiesFromFile = File.ReadAllLines("ProxyList.txt");
+                m_proxies.AddRange(proxiesFromFile);
+            }
         }
 
         public Image GetImage(string link)
