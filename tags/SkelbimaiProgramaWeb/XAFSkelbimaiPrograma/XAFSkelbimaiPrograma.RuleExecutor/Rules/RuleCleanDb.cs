0ea0a104f1d54eddd579d038450551fd71ffb284
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2013-11-25 09:02:28 +0000

    Pridėtas rulsas db valymui

---
+++ tags/SkelbimaiProgramaWeb/XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.RuleExecutor/Rules/RuleCleanDb.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Xpo;
+using XAFSkelbimaiPrograma.Module.BusinessObjects.ORMDataModelCode;
+
+namespace XAFSkelbimaiPrograma.RuleExecutor.Rules
+{
+    class RuleCleanDb : IRule
+    {
+        public void Execute()
+        {
+            Session session = new Session { ConnectionString = StaticData.CONNECTION_STRING };
+            List<string> queries = GetQueries();
+
+            foreach (string query in queries)
+            {
+                session.ExecuteNonQuery(query);
+            }
+            session.Disconnect();
+            session.Dispose();
+        }
+
+        private List<string> GetQueries()
+        {
+            List<string> result = new List<string>();
+            string queryLog = "delete FROM \"SKParserLog\" where \"GCRecord\" is not null";
+
+            DateTime dateMonthMinus = DateTime.Now.AddMonths(-1);
+            string queryAdvert = string.Format("delete FROM \"SKAdvert\" where \"FoundDate\" < '{0}' and \"Deleted\" = 1 and \"Mark\" = 0", dateMonthMinus);
+            string querySettings = "delete FROM \"SKUserSearchSettings\" where \"GCRecord\" is not null and \"Oid\" not in (select a.\"SearchSetting\" from \"SKAdvert\" a)";
+
+            result.Add(queryLog);
+            result.Add(queryAdvert);
+            result.Add(querySettings);
+
+            return result;
+
+        }
+    }
+}
