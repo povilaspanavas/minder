2bed89dd441040ff4e2520ae194e5f7f40054206
ign.bgd <ign.bgd@gmail.com>
2013-11-25 20:50:03 +0000

    Padarytas loginimas mailo ir proxy naudojimas autoplius

--- XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Parser/Helpers/LogDBHelper.cs
+++ XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Parser/Helpers/LogDBHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DevExpress.Data.Filtering;
 using DevExpress.Xpo;
 using XAFSkelbimaiPrograma.Module.BusinessObjects.ORMDataModelCode;
 using XAFSkelbimaiPrograma.Module.BusinessObjects.ORMDataModelCode.Objects;
@@ -22,6 +23,15 @@ namespace XAFSkelbimaiPrograma.Parser.Helpers
                 return;
 
             Session session = new Session { ConnectionString = StaticData.CONNECTION_STRING };
+            //patikriname dar db ar jau yra
+            XPCollection<SKParserLog> logCollection =
+                new XPCollection<SKParserLog>(session,
+                    CriteriaOperator.Parse(string.Format("Message = '{0}'", message)),
+                    null);
+
+            if (logCollection.Count != 0)
+                return;
+            
             SKParserLog logObj = new SKParserLog(session);
             logObj.Message = message;
             logObj.LogDate = DateTime.Now;
