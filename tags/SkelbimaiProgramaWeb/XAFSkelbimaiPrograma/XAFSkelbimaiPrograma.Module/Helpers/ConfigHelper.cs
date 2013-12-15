bf10dbd02d229fc65e7b2478414eff5e8cdc071c
ign.bgd@gmail.com <ign.bgd@gmail.com@01a90b2a-6592-0b12-695f-ea2d4c1986cf>
2013-12-15 12:11:30 +0000

    Perdaryta, kad configas realiu laiku būtų loadinmas iš web.config

---
+++ tags/SkelbimaiProgramaWeb/XAFSkelbimaiPrograma/XAFSkelbimaiPrograma.Module/Helpers/ConfigHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace XAFSkelbimaiPrograma.Module.Helpers
+{
+    public class ConfigHelper
+    {
+        private static string m_connectionString = null;
+
+        public string GetConnectionString()
+        {
+            try
+            {
+                if (m_connectionString != null)
+                    return m_connectionString;
+
+                string path = HttpContext.Current.Server.MapPath("/Web.config");
+                string allFile = File.ReadAllText(path);
+                string[] splited = Regex.Split(allFile, "<add name=\"ConnectionString\" connectionString=\"");
+                string[] splited2 = Regex.Split(splited[1], "\"");
+
+                m_connectionString = splited2[0];
+                return splited2[0];
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            
+        }
+    }
+}
